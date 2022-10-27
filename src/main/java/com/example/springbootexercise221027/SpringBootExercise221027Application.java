4de68502565a5bebc0aeba675942cd2adfa02650
pigeon2gugu <khn1135@gmail.com>
2022-10-27 23:05:16 +0900

    initial commit

---
+++ src/main/java/com/example/springbootexercise221027/SpringBootExercise221027Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootexercise221027;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExercise221027Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExercise221027Application.class, args);
+    }
+
+}
