16b9951a1ee952b63cd6560508ae94af8ad0e551
pigeon2gugu <khn1135@gmail.com>
2022-10-27 23:09:03 +0900

    feat: apply Swagger 3.0.0

---
+++ src/main/java/com/example/springbootexercise221027/configuration/SwaggerConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.springbootexercise221027.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+public class SwaggerConfiguration {
+
+    @Configuration
+    public class SwaggerConfig {
+
+        @Bean
+        public Docket api() {
+            return new Docket(DocumentationType.OAS_30)
+                    .select()
+                    .apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any())
+                    .build();
+        }
+    }
+}
